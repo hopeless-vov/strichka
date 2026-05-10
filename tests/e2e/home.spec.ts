961c9b9b92e2a2fa737c604e713a7dfcdc8d69ed
hopeless-vov <vov.bndrnk@gmail.com>
2026-05-10 17:00:05 +0200

    Update e2e test

--- tests/e2e/home.spec.ts
+++ tests/e2e/home.spec.ts
@@ -1,6 +1,7 @@
-import { test, expect } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
 test('home page loads', async ({ page }) => {
   await page.goto('/')
-  await expect(page.locator('h1')).toContainText('strichka')
+  await expect(page.locator('nav')).toContainText('strichka')
+  await expect(page.locator('section').first()).toBeVisible()
 })
