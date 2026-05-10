5ddb6ab9c59b4b703939464136b9e4acc26fb0b5
hopeless-vov <vov.bndrnk@gmail.com>
2026-05-10 16:57:16 +0200

    Fix types in config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
